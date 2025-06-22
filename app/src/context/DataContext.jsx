d4b22b9e5089e3cc850c6c053792fd7927387d3d
Mohammad Danish <m.danish00707@gmail.com>
2025-06-23 01:44:03 +0530

    Added

--- app/src/context/DataContext.jsx
+++ app/src/context/DataContext.jsx
@@ -5,6 +5,7 @@ const DataContext = createContext()
 
 export const DataProvider = ({ children }) => {
   const [loading, setLoading] = useState(false)
+  // ✅ Use a more descriptive name for the context
   // const BASE_URL = 'http://localhost:5000'
   const BASE_URL = 'https://project-2-backend-1hun.onrender.com'
 
